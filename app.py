defa769d9acb8e5812eefe4106c03414d4df7bfd
Danny McClafferty <danny@Dannys-MacBook-Pro.local>
2018-04-14 19:48:43 -0400

    changes

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template, json, request
 import mysql.connector
 from flaskext.mysql import MySQL
+#import pickleimport 
+#import os
+##import json
+##import string
+#import nltk
+#import pandas as pd
+
 app = Flask(__name__)
 
 # mysql = MySQL()
@@ -15,6 +22,11 @@ mysql.init_app(app)'''
 cnx = mysql.connector.connect(user='root', password='root',
                               host='127.0.0.1',
                               database='HackDartmouth')
+
+#filename_model = 'model.pkl'
+#loaded_model = pickle.load(open(filename_model, 'rb'))
+#filename_vec = 'vectorizor.pkl'
+#vectorizor = pickle.load(open(filename_vec, 'rb'))
 
 # conn = mysql.connect()
 
@@ -32,6 +44,11 @@ def contact():
 @app.route('/signUp',methods=['POST'])
 def signUp():
      _comment = request.form['inputComment']
+
+     # parse comment 
+
+     # features = vect.transform([_comment])
+     # predicted_rating = loaded_model.predict(features)
 
      #result = cursor.execute('insert into comments (comment) values ('+ _comment +');')
 
@@ -51,9 +68,9 @@ def signUp():
      # data = cursor.fetchall()
 
      if _comment:
-     	return json.dumps({'html':result})
+        return json.dumps({'html':result})
      else:
-     	return json.dumps({'html':'enter the required fields'})
+        return json.dumps({'html':'enter the required fields'})
 
 
 
