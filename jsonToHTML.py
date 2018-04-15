950d91031b5afec9536a67297b913ed4ffe34a5a
Danny McClafferty <danny@Dannys-MacBook-Pro.local>
2018-04-15 03:18:40 -0400

    dev

--- jsonToHTML.py
+++ jsonToHTML.py
@@ -1,2 +1,12 @@
 def jsonToHTML(input):
-	raw = [{'stuff', 'attr'}, {'stuff1', 'attr1'}]
+     html = '<table class="table table-striped" style="width:100%">'
+     html+="<tr>"
+     for key in input[0]:
+          html+='<th>' + str(key) + "</th>"
+     html+="</tr>"
+     for row in range(0, len(input)):
+          html+="<tr>"
+          for key in input[row]:
+               html+='<td scope="col">' + str(input[row][key]) + "</td>"
+          html+="</tr>"
+     return html
