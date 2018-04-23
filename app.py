9fec57b7a53c7fe5b1857cea11c85ab9126e818e
Ewen <wang.yuxu@husky.neu.edu>
2018-04-22 22:16:10 -0400

    mods

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ import logging
 
 #import pandas as pd
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__, template_folder='Templates')
 
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.ERROR)
